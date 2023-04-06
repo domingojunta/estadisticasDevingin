ea7087f9be1f41c9695bf75e30a57274cb0003a9
domingojunta <domingojunta@gmail.com>
2023-04-06 15:21:27 +0200

    Tras error push

--- src/main/java/es/domingojunta/Application.java
+++ src/main/java/es/domingojunta/Application.java
@@ -3,10 +3,9 @@ package es.domingojunta;
 import java.awt.EventQueue;
 
 import javax.swing.UIManager;
-<<<<<<< HEAD
+
 import javax.swing.UnsupportedLookAndFeelException;
-=======
->>>>>>> a9f27265802f2563a10bb402741b1c581cedb7be
+
 
 import es.domingojunta.views.HomeWindow;
 
@@ -17,10 +16,9 @@ public class Application {
 	public static void main(String[] args) {
 		
 		String sistemaOperativo = System.getProperty("os.name");
-<<<<<<< HEAD
+
 		System.out.println(sistemaOperativo);
-=======
->>>>>>> a9f27265802f2563a10bb402741b1c581cedb7be
+
 		
 		switch (sistemaOperativo) {
 		case "Mac OS X": 
@@ -31,36 +29,30 @@ public class Application {
 			System.out.println("Look and Field de Mac Os...");
 			break;
 		
-<<<<<<< HEAD
+
 		case "Windows","Windows 10","Windows 11" :
 			
 			try {
 //				UIManager.setLookAndFeel("javax.swing.plaf.windows.WindowsLookAndFeel");
 				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-=======
-		case "Windows" :
-			
-			try {
-				UIManager.setLookAndFeel("javax.swing.plaf.windows.WindowsLookAndFeel");
->>>>>>> a9f27265802f2563a10bb402741b1c581cedb7be
+				break;
+
 			} catch (Exception e) {
+				System.out.println("Look and Field de Windows...");
 				break;
 			}
-			System.out.println("Look and Field de Windows...");
-			break;
+			
 		
 		
 		default:
 			System.out.println("Look and Field de por defecto");
-<<<<<<< HEAD
+
 			try {
 				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 			} catch (Exception e) {
 				System.out.println("Look and Field by default");
 			} 
-=======
-			
->>>>>>> a9f27265802f2563a10bb402741b1c581cedb7be
+
 		}
 		
 		
